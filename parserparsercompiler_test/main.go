59646f64cc66832feff9c565191c42e9cd021337
David Matscheko <d.matscheko@analog-bit.com>
2021-02-27 12:52:11 +0100

    preparation for tag to contain an ID and a script

--- parserparsercompiler_test/main.go
+++ parserparsercompiler_test/main.go
@@ -48,7 +48,7 @@ var (
 		name <"collect1">  = ( small | caps ) { small | caps | digit | "_" } .
 		text <"collect2"> = "\"" - { small | caps | digit | special } "\"" + .
 
-		tag  = "<" text { ";" text } ">" .
+		tag  = "<" "id" "=" text ";" "code" "=" text ">" .
 
 		digit = "0" | "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" .
 		small = "a" | "b" | "c" | "d" | "e" | "f" | "g" | "h" | "i" | "j" | "k" | "l" | "m" | "n" | "o" | "p" | "q" | "r" | "s" | "t" | "u" | "v" | "w" | "x" | "y" | "z" .
